0498f165e1528e03f78631273aca86e77ce21da8
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-03-05 15:31:30 -0500

    Added commands utils

---
+++ commands/utils/commands.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'commands',
+    description: 'list all commands',
+    execute(message, args, commandName, client){
+        const aliases = require('./join').aliases
+        let reply = "Here is a list of all CatimoMeme's commands:\n\n";
+        let count = 1;
+
+        for(let name of aliases){
+            reply += `${count++}- #${name}\n`;
+        }
+
+        for(let command of client.commands){
+            console.log(command)
+            reply += `${count++}- #${command[0]}\n`
+        }
+
+        message.channel.send(reply);
+    }
+}
--- commands/utils/join.js
+++ commands/utils/join.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 module.exports = {
     name: 'join',
     description: 'joins voice channel',
-    aliases: ['nani', 'duel', 'barrel', 'weed', 'wow', 'xfile', 'acabou', 'no'],
-    async execute(message, args, commandName, client){
+    aliases: ['nani', 'duel', 'barrel', 'weed', 'hello', 'wow', 'xfile', 'acabou', 'no', 'shit', 'finish', 'objection', 'sad', 'do', 'choppa'],
+    async execute(message, args, commandName){
 
         const voiceChannel = await message.member.voice.channel;
         if(!voiceChannel) return message.reply('You have to be on a voice channel to call this command!');
@@ -13,7 +13,7 @@ module.exports = {
 
         if(commandName){
             const sound = await fs.createReadStream(__dirname.replace('\\commands\\utils', "") + `\\audio\\${commandName}\\${commandName}_sound.mp3`);
-            await connection.play(sound, {seek: 0, volume: 0.5});
+            await connection.play(sound, {seek: 0, volume: 0.3});
         }
     }
 }
